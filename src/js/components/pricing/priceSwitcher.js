ec62d528639e4f665bea99e0b028747c20420658
Elizaveta <ivanova-liza@ya.ru>
2024-10-19 18:09:39 +0300

    Created pricing:faq

--- src/js/components/pricing/priceSwitcher.js
+++ src/js/components/pricing/priceSwitcher.js
@@ -4,39 +4,51 @@ export const usePriceSwitcher = () => {
   const popularPrice = document.querySelector('[data-price="popular"]');
   const enterprisePrice = document.querySelector('[data-price="enterprise"]');
 
+  const starterPeriod = starterPrice.nextElementSibling;
+  const popularPeriod = popularPrice.nextElementSibling;
+  const enterprisePeriod = enterprisePrice.nextElementSibling;
+
   const priceList = {
     starter: {
       default: 120,
-      witchSale: 100,
+      annual: 120 * 12 * 0.8,
     },
     popular: {
       default: 1680,
-      witchSale: 1400,
+      annual: 1680 * 12 * 0.8,
     },
     enterprise: {
       default: 2520,
-      witchSale: 2100,
+      annual: 2520 * 12 * 0.8,
     },
   };
 
-  const setPricesWitchSale = () => {
-    starterPrice.textContent = priceList.starter.witchSale;
-    popularPrice.textContent = priceList.popular.witchSale;
-    enterprisePrice.textContent = priceList.enterprise.witchSale;
+  const setPricesAnnual = () => {
+    starterPrice.textContent = priceList.starter.annual;
+    popularPrice.textContent = priceList.popular.annual;
+    enterprisePrice.textContent = priceList.enterprise.annual;
+
+    starterPeriod.textContent = "/yr";
+    popularPeriod.textContent = "/yr";
+    enterprisePeriod.textContent = "/yr";
   };
 
   const setDefaultPrices = () => {
     starterPrice.textContent = priceList.starter.default;
     popularPrice.textContent = priceList.popular.default;
     enterprisePrice.textContent = priceList.enterprise.default;
+
+    starterPeriod.textContent = "/mo";
+    popularPeriod.textContent = "/mo";
+    enterprisePeriod.textContent = "/mo";
   };
 
   switcher.checked = true;
-  setPricesWitchSale();
+  setPricesAnnual();
 
   switcher.addEventListener('click', () => {
     if (switcher.checked) {
-      setPricesWitchSale();
+      setPricesAnnual();
     } else {
       setDefaultPrices();
     }
--- src/js/pricing.js
+++ src/js/pricing.js
@@ -3,6 +3,7 @@ import '../assets/scss/pricing.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import './components/accordion.js';
 import { usePriceSwitcher } from './components/pricing/priceSwitcher.js';
 
 useTheme();
